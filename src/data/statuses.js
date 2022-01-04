4ae792161b764becacd49ec900f3f2e01913dd88
Alexander Kozlov <kozloww1508@yandex.ru>
2022-01-04 12:05:02 +0300

    fix database

---
+++ src/data/statuses.js
@@ -0,0 +1,19 @@
+export const statuses = {
+    'added': {
+        statusStyle: "primary",
+        statusText: "Создан"
+    },
+    'canceled': {
+        statusStyle: "danger",
+        statusText: "Отменен"
+    },
+    'inWork': {
+        statusStyle: "warning",
+        statusText: "Взято в работу"
+    },
+    'ended': {
+        statusStyle: "final",
+        statusText: "Завершено"
+    },
+    
+}
